7a687a32ad8880cd01e1095195f783313dd155b9
MarkupMastermind <amritsamra424@gmail.com>
2024-07-30 19:02:18 -0700

    completed

---
+++ src/Components/AddId.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+const AddId = () => {
+  return (
+    <div>
+        <NavBar/>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <div className="row g-3">
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <label htmlFor="" className="form-label">Id Name</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                            <button className="p-2 btn btn-primary">Add</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AddId
